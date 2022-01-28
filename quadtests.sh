43506a557ca69228c8e45ec10a84082f578c906e
Maxim <89240935+Maxmosk@users.noreply.github.com>
2022-01-29 00:53:14 +0300

    Better shell tests

--- quadtests.sh
+++ quadtests.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+I=1
 while IFS=% read -r source roots; do
         res=$(echo $source | ./quad)
         res=$(echo "$res" | tail -n +2)
         if [ x"$roots" != x"$res" ]; then
-                echo TEST $source FAILED: expexted "$roots", got "$res"
+                echo TEST $I '('$source')' FAILED: expexted "$roots", got "$res"
+        else
+                echo TEST $I PASSED
         fi
+        I=$((I+1))
 done <<END
         1 2 1%-1.000
         1 4 3%-3.000 -1.000
