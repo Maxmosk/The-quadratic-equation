76266d746c5f713e6ad088831c88684b5ecf048a
Maxim <maxmosk@list.ru>
2022-07-21 16:43:26 +0300

    Add solvers by to ways

--- tests.py
+++ tests.py
@@ -3,34 +3,54 @@ import sympy
 import subprocess
 import random
 
+
+def generate_equation(n):
+    return map(float, [random.triangular(low_coeff, high_coeff, mode_coeff) for i in range(n)])
+
+def solve_symbolic(a, b, c):
+    x = sympy.var('x')
+    f = a*x**2 + b*x + c
+    
+    solution = sympy.solve(f, x)
+
+    have_roots = True
+    for root in solution:
+        if not root.is_real:
+            have_roots = False
+
+    if have_roots:
+        return map(float, solution)
+    else:
+        return None
+
+def solve_execuatble(a, b, c, solver):
+    solver_output = subprocess.check_output(
+        solver,
+        input=f"{a} {b} {c}",
+        text=True
+        )
+
+    return solver_output.split('\n')[1]
+
+
 high_coeff = 100500
 low_coeff = -high_coeff
 mode_coeff = 1050
 accuracy = 0.001
 
-a = random.triangular(low_coeff, high_coeff, mode_coeff)
-b = random.triangular(low_coeff, high_coeff, mode_coeff)
-c = random.triangular(low_coeff, high_coeff, mode_coeff)
+
+a, b, c = generate_equation(3)
 print(a, b, c)
 
-x = sympy.var('x')
-f = a*x**2 + b*x + c
+res = solve_symbolic(a, b, c)
+if res != None:
+    for root in res:
+        print(root, end=' ')
+    print('')
+else:
+    print("No solutions")
 
-solution = sympy.solve(f, x)
 
-have_roots = True
-for root in solution:
-    if not root.is_real:
-        have_roots = False
+print(solve_execuatble(a, b, c, "./quad"))
 
-if have_roots:
-    sympy.pprint(solution)
-
-solver_output = subprocess.check_output(
-        "./quad",
-        input=f"{a} {b} {c}",
-        text=True
-        )
-
-print(solver_output)
 
