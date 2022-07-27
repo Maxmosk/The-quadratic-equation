e8c27f531c01f8de23d6a41cc8ef29c1de601b43
Maxim <maxmosk@list.ru>
2022-07-27 13:09:19 +0300

    Better codestyle in solver

--- src/code/solver.c
+++ src/code/solver.c
@@ -17,22 +17,22 @@ int is_zero (double nmb)
 
 void print_solution (int status, double sol_1, double sol_2)
 {
-    switch(status)
+    switch (status)
     {
         case ZERO_SOL:
-            printf("No solutions\n");
+            printf ("No solutions\n");
             break;
             
         case ONE_SOL:
-            printf("%.3lf\n", sol_1);
+            printf ("%.3lf\n", sol_1);
             break;
             
         case TWO_SOL:
-            printf("%.3lf %.3lf\n", sol_1, sol_2);
+            printf ("%.3lf %.3lf\n", sol_1, sol_2);
             break;
             
         case INF_SOLS:
-            printf("All real numbers\n");
+            printf ("All real numbers\n");
             break;
             
         default:
@@ -52,7 +52,7 @@ solver_code solve_linear(double k, double b, double *sol)
         return SOL_ERR;
     }
     
-    bool corr_data = ( isfinite(k) && isfinite(b) );
+    bool corr_data = ( isfinite (k) && isfinite (b) );
     
     assert(corr_data);
     if (!corr_data)
@@ -61,13 +61,13 @@ solver_code solve_linear(double k, double b, double *sol)
         return SOL_ERR;
     }
     
-    if (!is_zero(k))
+    if (!is_zero (k))
         {
             *sol = -b/k;
             return ONE_SOL;
         }
         
-        else if (is_zero(b) && is_zero(k))
+        else if (is_zero (b) && is_zero (k))
             return INF_SOLS;
         
         else
@@ -77,8 +77,8 @@ solver_code solve_linear(double k, double b, double *sol)
 solver_code solve_quad (double a, double b, double c, double *sol_1, double *sol_2)
 {
     /* checking the pointer for a null pointer */
-    assert( !(!sol_1 && !sol_2) );
-    if ( (!sol_1 && !sol_2) )
+    assert(sol_1 && sol_2);
+    if (!sol_1 || !sol_2)
     {
         errno = EFAULT;
         return SOL_ERR;
@@ -87,7 +87,7 @@ solver_code solve_quad (double a, double b, double c, double *sol_1, double *sol
     *sol_1 = NAN;
     *sol_2 = NAN;
     
-    bool corr_data = ( isfinite(a) && isfinite(b) && isfinite(c) );
+    bool corr_data = isfinite (a) && isfinite (b) && isfinite (c);
     
     assert(corr_data);
     if (!corr_data)
@@ -97,15 +97,15 @@ solver_code solve_quad (double a, double b, double c, double *sol_1, double *sol
     }
     
     /* in the case of a linear equation */
-    if (is_zero(a))
+    if (is_zero (a))
     {
-        return solve_linear(b, c, sol_1);
+        return solve_linear (b, c, sol_1);
     }
     
     /* solving a quadratic equation with a discriminant */
     
     double b_sq = b*b;
-    bool corr_b_sq = isfinite(b_sq);
+    bool corr_b_sq = isfinite (b_sq);
     
     assert(corr_b_sq);
     if (!corr_b_sq)
@@ -115,20 +115,20 @@ solver_code solve_quad (double a, double b, double c, double *sol_1, double *sol
     }
     
     double ac = -4*a*c;
-    bool corr_b_ac = isfinite(ac);
+    bool corr_b_ac = isfinite (ac);
     
     assert(corr_b_ac);
-    if (! corr_b_ac)
+    if (!corr_b_ac)
     {
         errno = ERANGE;
         return SOL_ERR;
     }
     
     double D = b_sq + ac;
-    bool corrD = isfinite(D);
+    bool corrD = isfinite (D);
     
     assert(corrD);
-    if (! corrD)
+    if (!corrD)
     {
         errno = ERANGE;
         return SOL_ERR;
@@ -136,14 +136,14 @@ solver_code solve_quad (double a, double b, double c, double *sol_1, double *sol
     
     if (D < 0)
         return ZERO_SOL;
-    else if (is_zero(D))
+    else if (is_zero (D))
     {
         *sol_1 = -b / (2*a);
         return ONE_SOL;
     }
     else
     {
-        double sD = sqrt(D);
+        double sD = sqrt (D);
         *sol_1 = (-b - sD) / (2*a);
         *sol_2 = (-b + sD) / (2*a);
         return TWO_SOL;
